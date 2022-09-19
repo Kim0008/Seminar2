0ab4bd2048c22de4c5720e4c897d4608e0844ad5
KIM <kim.02@mail.ru>
2022-09-19 22:14:16 +0300

    до

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. (метод)
-int CreateNumber()
+ int CreateNumber()
 {
     int number = new Random().Next(10, 100);
     return number;
 }
 
-int MaxDigit(int number)
+ int MaxDigit(int number)
 {
     int Decinal = number / 10;
     int Ed = number % 10;
