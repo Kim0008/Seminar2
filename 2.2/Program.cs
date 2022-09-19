a808143d968c44c609d860afd1c8dfcc70124725
KIM <kim.02@mail.ru>
2022-09-19 21:24:00 +0300

    выводит случайное трёхзначное число и удаляет вторую цифру этого числа

---
+++ 2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+int CreateNumber()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+int DelSecondDigit(int number)
+{
+    int S = number / 100;
+    int E = number % 10;
+    int numberNew = S *10 + E;
+    return numberNew;
+}
+int a = CreateNumber();
+Console.WriteLine(a);
+int numberNew = DelSecondDigit(a);
+Console.WriteLine(numberNew);
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа. (метод)
+int CreateNumber()
+{
+    int number = new Random().Next(10, 100);
+    return number;
+}
+
+int MaxDigit(int number)
+{
+    int Decinal = number / 10;
+    int Ed = number % 10;
+    if (Decinal >= Ed)
+
+    {
+        return Decinal;
+    }
+    else
+    {
+        return Ed;
+    }
+    
+    int a = CreateNumber();
+    Console.WriteLine(a);
+    int maxDigit = MaxDigit(a);
+}
